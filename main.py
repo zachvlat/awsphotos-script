20fb7552f104a4658688b671d63b0c2d7500b4f1
zachvlat <zachariasvlatakis@gmail.com>
2022-12-01 14:41:30 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+print("this is my aws-photo-app-lazy-invalidation-builder");
+
+print("    .")
+print("   ...")
+print("  ......")
+print(" ........")
+print(".......... MERRY CHRISTMAS")
+print("      ")
+
+photoCode = input("What is the photo code?: ")
+
+print("      ")
+
+print("/verybig/"+photoCode+"@4.JPG")
+print("/verybig/"+photoCode+"@4.jpg")
+print("/small/"+photoCode+"@4.JPG")
+print("/small/"+photoCode+"@4.jpg")
+
+print("/verybig/"+photoCode+"@6.JPG")
+print("/verybig/"+photoCode+"@6.jpg")
+print("/small/"+photoCode+"@6.JPG")
+print("/small/"+photoCode+"@6.jpg")
+
+print("/verybig/"+photoCode+"@5.JPG")
+print("/verybig/"+photoCode+"@5.jpg")
+print("/small/"+photoCode+"@5.JPG")
+print("/small/"+photoCode+"@5.jpg")
+
+print("/verybig/"+photoCode+"@1F.JPG")
+print("/verybig/"+photoCode+"@1F.jpg")
+print("/small/"+photoCode+"@1F.JPG")
+print("/small/"+photoCode+"@1F.jpg")
+
+print("/verybig/"+photoCode+"@2F.JPG")
+print("/verybig/"+photoCode+"@2F.jpg")
+print("/small/"+photoCode+"@2F.JPG")
+print("/small/"+photoCode+"@2F.jpg")
