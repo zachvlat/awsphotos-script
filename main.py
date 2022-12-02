f7073d9b03e4d7b549d405b1ad17fb9399a9611e
zachvlat <zachariasvlatakis@gmail.com>
2022-12-02 12:18:05 +0200

    couple changes

--- main.py
+++ main.py
@@ -11,27 +11,31 @@ print("      ")
 photoCode = input("What is the photo code?: ")
 print("      ")
 
-print("/verybig/"+photoCode+"@4.JPG")
-print("/verybig/"+photoCode+"@4.jpg")
-print("/small/"+photoCode+"@4.JPG")
-print("/small/"+photoCode+"@4.jpg")
-
-print("/verybig/"+photoCode+"@6.JPG")
-print("/verybig/"+photoCode+"@6.jpg")
-print("/small/"+photoCode+"@6.JPG")
-print("/small/"+photoCode+"@6.jpg")
-
-print("/verybig/"+photoCode+"@5.JPG")
-print("/verybig/"+photoCode+"@5.jpg")
-print("/small/"+photoCode+"@5.JPG")
-print("/small/"+photoCode+"@5.jpg")
-
-print("/verybig/"+photoCode+"@1F.JPG")
-print("/verybig/"+photoCode+"@1F.jpg")
-print("/small/"+photoCode+"@1F.JPG")
-print("/small/"+photoCode+"@1F.jpg")
-
-print("/verybig/"+photoCode+"@2F.JPG")
-print("/verybig/"+photoCode+"@2F.jpg")
-print("/small/"+photoCode+"@2F.JPG")
-print("/small/"+photoCode+"@2F.jpg")
+chooseSizes = input("What size do you want?: ")
+if chooseSizes == "4" :
+    print("/verybig/"+photoCode+"@"+chooseSizes+".JPG")
+    print("/verybig/"+photoCode+"@"+chooseSizes+".jpg")
+    print("/small/"+photoCode+"@"+chooseSizes+".JPG")
+    print("/small/"+photoCode+"@"+chooseSizes+".jpg")
+elif chooseSizes == "6":
+    print("/verybig/"+photoCode+"@6.JPG")
+    print("/verybig/"+photoCode+"@6.jpg")
+    print("/small/"+photoCode+"@6.JPG")
+    print("/small/"+photoCode+"@6.jpg")
+elif chooseSizes == "5":
+    print("/verybig/"+photoCode+"@5.JPG")
+    print("/verybig/"+photoCode+"@5.jpg")
+    print("/small/"+photoCode+"@5.JPG")
+    print("/small/"+photoCode+"@5.jpg")
+elif chooseSizes == "1F":
+    print("/verybig/"+photoCode+"@1F.JPG")
+    print("/verybig/"+photoCode+"@1F.jpg")
+    print("/small/"+photoCode+"@1F.JPG")
+    print("/small/"+photoCode+"@1F.jpg")
+elif chooseSizes == "2F":
+    print("/verybig/"+photoCode+"@2F.JPG")
+    print("/verybig/"+photoCode+"@2F.jpg")
+    print("/small/"+photoCode+"@2F.JPG")
+    print("/small/"+photoCode+"@2F.jpg")
+else:
+    print("choose the right size please!")
